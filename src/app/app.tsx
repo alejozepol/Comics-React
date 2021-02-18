6b5441b0122f7e82ccffcb7dc1adedc5c32b18d8
Alejandro Lopez Ramirez <alejozepol@gmail.com>
2021-02-17 21:34:49 -0500

    desing's card

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,22 +6,28 @@ import "./app.scss";
 
 export const App = () => {
   const [publication, setPublication] = useState<Publication[]>([]);
-  const [ref, setRef] = useState<number>(15)
+  const [ref, setRef] = useState<number>(15);
 
   useEffect(async () => {
-    const _body:Publication[] = []
-    const {error, body} = await API.getAllAPI(10);
-    if(!error){
+    const _body: Publication[] = [];
+    const { error, body } = await API.getAllAPI(10);
+    if (!error) {
       setPublication(body);
     }
-    console.log(body)
+    console.log(body);
   }, []);
 
   return (
-    <section className='app'>
-      {
-        publication.map((item) =><Comic key={item.num} publication={item} /> )
-      }
+    <section className="app">
+      <div className='app__title'>
+        <h2>xkcd</h2>
+        <p>A webcomic of romance,sarcasm, math, and language.</p>
+      </div>
+      <section className="app__content">
+        {publication.map((item) => (
+          <Comic key={item.num} publication={item} />
+        ))}
+      </section>
     </section>
   );
 };
--- src/components/comic/index.tsx
+++ src/components/comic/index.tsx
@@ -3,11 +3,35 @@ import { Publication } from "../../lib/Publication.interface";
 import "./comic.scss";
 
 const Comic = ({ publication = {} as Publication }) => {
+  const namemMonth = (numMonth: number) => {
+    console.log(numMonth);
+    const month = [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+    ];
+    if (numMonth <= 12 && numMonth >= 1) {
+      return month[numMonth - 1];
+    } else {
+      return month[0];
+    }
+  };
   return (
     <section className="Comic">
       <h2 className="Comic__title">{publication.title}</h2>
-      <h4 className="Comic__date">{`${publication.month} ${publication.day}`}</h4>
-      <span className="Comic__ref">{publication.num}num</span>
+      <span className="Comic__date">{`${publication.day} de ${namemMonth(
+        Number(publication.month)
+      )} `}</span>
+      <span className="Comic__ref">ref: {publication.num}</span>
       <div className="Comic__img">
         <img
           className="Comic__img-src"
@@ -15,8 +39,16 @@ const Comic = ({ publication = {} as Publication }) => {
           alt={publication.alt}
         />
       </div>
+      <p className="Comic__text">{publication.alt}</p>
       <div className="Comic__actions">
-        <p>d</p>
+        <div className='Comic__actions-like'>
+          <span className="material-icons">thumb_up_alt</span>
+          <p>Me gusta</p>
+        </div>
+        <div className='Comic__actions-share'>
+          <span className="material-icons">share</span>
+          <p>Compartir</p>
+        </div>
       </div>
     </section>
   );
